52e673ba6bb475118de173af7d3c8a822359b531
kimSolee <ahhasolee@naver.com>
2021-08-27 16:06:00 +0900

    Object를 인자로 한 remove
    Object를 parameter로 한 remove method 기능 구현

--- DataStructure/src/com/datastructure/solist/TestMakedArrayList.java
+++ DataStructure/src/com/datastructure/solist/TestMakedArrayList.java
@@ -6,11 +6,11 @@ public class TestMakedArrayList {
 		
 		List makedArrayList = new SoList();
 		
-		makedArrayList.add(1);
-		makedArrayList.add(2);
-		makedArrayList.add(3);
-		makedArrayList.add(4);
-		makedArrayList.add(5);
+		makedArrayList.add("처음 add 시, index 0 요소");
+		makedArrayList.add("처음 add 시, index 1 요소");
+		makedArrayList.add("처음 add 시, index 2 요소");
+		makedArrayList.add("처음 add 시, index 3 요소");
+		makedArrayList.add("처음 add 시, index 4 요소");
 
 		//add test
 		System.out.println("== 만든 arrayList에 add method를 통해 추가한 요소를 확인해봅니다. ==");
@@ -26,12 +26,21 @@ public class TestMakedArrayList {
 		//remove test
 		System.out.println("만든 arrayList에 remove method를 통해 삭제한 요소를 확인해봅니다.");
 		
+		System.out.println("1. index를 인자로 하여 삭제해보기(remove(2 ))");
 		makedArrayList.remove(2);
 		System.out.println(makedArrayList.size());
 		System.out.println(makedArrayList.get(0));
 		System.out.println(makedArrayList.get(1));
 		System.out.println(makedArrayList.get(2));
 		System.out.println(makedArrayList.get(3));
+		
+		System.out.println("2. 배열내 Object를 인자로 하여 삭제해보기");
+		makedArrayList.remove(3);
+		System.out.println(makedArrayList.size());
+		System.out.println(makedArrayList.get(0));
+		System.out.println(makedArrayList.get(1));
+		System.out.println(makedArrayList.get(2));
+		
 	}
 
 }
