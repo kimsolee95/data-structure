506289695283fa711463e475758df2995916b49a
kimSolee <ahhasolee@naver.com>
2021-08-29 20:58:05 +0900

    배열자료형지정test
    test code작성

--- DataStructure/src/com/datastructure/solist/TestMakedArrayList.java
+++ DataStructure/src/com/datastructure/solist/TestMakedArrayList.java
@@ -4,13 +4,21 @@ public class TestMakedArrayList {
 	
 	public static void main(String[] args) {
 		
-		List makedArrayList = new SoList();
+		List<String> makedArrayList = new SoList<>(); //String만 담는 배열
+		List<Integer> arrayList2 = new SoList<>(); //int형만 담는 배열
 		
 		makedArrayList.add("처음 add 시, index 0 요소");
 		makedArrayList.add("처음 add 시, index 1 요소");
 		makedArrayList.add("처음 add 시, index 2 요소");
 		makedArrayList.add("처음 add 시, index 3 요소");
 		makedArrayList.add("처음 add 시, index 4 요소");
+		
+		arrayList2.add(1);
+		arrayList2.add(2);
+		arrayList2.add(3);
+		arrayList2.add(4);
+		arrayList2.add(5);
+		
 
 		//add test
 		System.out.println("== 만든 arrayList에 add method를 통해 추가한 요소를 확인해봅니다. ==");
@@ -20,8 +28,18 @@ public class TestMakedArrayList {
 		System.out.println(makedArrayList.get(3));
 		System.out.println(makedArrayList.get(4));
 
-		System.out.println("== 만든 arrayList에 add method를 통해 추가된 요소의 개수를 확인해봅니다. ==");
+		System.out.println(arrayList2.get(0));
+		System.out.println(arrayList2.get(1));
+		System.out.println(arrayList2.get(2));
+		System.out.println(arrayList2.get(3));
+		System.out.println(arrayList2.get(4));
+		
+		System.out.println("== 만든 arrayList(String형)에 add method를 통해 추가된 요소의 개수를 확인해봅니다. ==");
 		System.out.println(makedArrayList.size());
+		
+		System.out.println("== 만든 arrayList(int형)에 add method를 통해 추가된 요소의 개수를 확인해봅니다. ==");
+		System.out.println(arrayList2.size());
+		
 		
 		//remove test
 		System.out.println("만든 arrayList에 remove method를 통해 삭제한 요소를 확인해봅니다.");
